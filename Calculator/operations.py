9bf1911c84e92a9db092b0d1e8e21154d3a90120
Armadz <Sehoban@gmail.com>
2017-10-15 22:03:47 -0500

    Added Subtract, Multiply, Divide operations

--- Calculator/operations.py
+++ Calculator/operations.py
@@ -7,3 +7,15 @@ Class that contains the math operations used by Calculator
 
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return float(a) / b
+
+    #To do: Add error if div by zero
