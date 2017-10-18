37813edfb538adef8e86a1aee53a7b2202928cdc
Armadz <Sehoban@gmail.com>
2017-10-17 21:44:58 -0500

    Added Operations
    *Floor
    *Ceiling
    *Power(postive int powers)

--- Calculator/operations.py
+++ Calculator/operations.py
@@ -19,3 +19,32 @@ def divide(a, b):
         return float(a) / b
 
     #To do: Add error if div by zero
+
+def abs_val(a):
+    if a < 0:
+        return a*-1
+    else:
+        return a
+
+def floor(a):
+    if a<0:
+        return int(a) - 1
+    else:
+        return int(a)
+
+def ceiling(a):
+    if a < 0:
+        return int(a)
+    elif a % 1 == 0:
+        return a
+    else:
+        return int(a) + 1
+
+def power(a,b):
+    #a raised to the power of b
+    c = a
+    for x in range(1,b):
+        a = a*c
+    return a
+    # only works for int powers
+    # meed to fix for power of 0
