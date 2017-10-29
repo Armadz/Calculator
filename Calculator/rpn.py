a46f0ef08e78ee20c4c87531b27d2e232da011d8
Armadz <Sehoban@gmail.com>
2017-10-29 16:05:13 -0500

    Reverse polish notation evalutor
    *work in progress

---
+++ Calculator/rpn.py
@@ -0,0 +1,11 @@
+"""
+
+Class that contains the function for evaluating a list of strings in Reverse Polish Notation
+"""
+
+from Calculator import operations
+
+def rnp(eq):
+    for i in eq:
+        if float(i) == float:
+            evstack.append(i)
