a0ed16d4ebc4713008372fbc28071e8bdc48706c
Armadz <Sehoban@gmail.com>
2017-11-06 10:35:01 -0600

    Added power test
    *added test to make sure power function works as intended in reverse
    polish notation

--- tests/test_rpn.py
+++ tests/test_rpn.py
@@ -5,3 +5,6 @@ class TestRPN(TestCase):
 
     def test_rpn_eval(self):
         assert rpn.rpn_eval(["3" , "2", "*", "11" , "-"]) == -5.0
+
+    def test_rpn_powers(self):
+        assert rpn.rpn_eval(["3","4","**", "2" , "3" , "-", "/"])==-81.0
