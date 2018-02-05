2fa2d77595fe240baa825c88d3ae9f1b094220f8
ProfOak <OpenProfOak@gmail.com>
2018-02-04 18:24:40 -0600

    feat(main): add skeleton interface for main function

--- bin/calculator.py
+++ bin/calculator.py
@@ -1,3 +1,38 @@
-from Calculator import operations
+"""calculator
 
-print(operations.add(1,2))
+Perform calculations on the command line
+"""
+
+import argparse
+
+def main():
+    """Main function"""
+
+    args = parse_args()
+
+    user_input = None
+    while user_input != 'q':
+
+        user_input = input('> ')
+        if args.rpn:
+            # Do RPN calculation
+            # rpn(user_input)
+            pass
+        else:
+            # do infix calculation
+            # infix(user_input)
+            pass
+
+def parse_args():
+    """Parse command line arguments"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r',
+                        '--rpn',
+                        default=False,
+                        action='store_true',
+                        help='Reverse Polish Notation (postfix notation)')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
