9d8e75a2a39e735b7b15294ef595cc0bfe94209a
Armadz <Sehoban@gmail.com>
2018-05-23 20:43:01 -0500

    Changed shuntingyard alg to accept a list of Strings

--- tests/test_infix_to_rpn.py
+++ tests/test_infix_to_rpn.py
@@ -28,8 +28,8 @@ def test_op_switch(test_input_1, test_input_2, expected):
     assert infix_to_rpn.op_switch(test_input_1, test_input_2) == expected
 
 @pytest.mark.parametrize('test_input, expected', [
-        ([ 3.0 , '+', 4.0, '*', 2.0, '/', '(', 1.0, '-', 5.0, ')', '^', 2.0,
-	'^', 3.0], [3.0, 4.0, 2.0, '*', 1.0, 5.0, '-', 2.0, 3.0, '^', '^', '/',
+        ([ '3' , '+', '4', '*', '2', '/', '(', '1', '-', '5', ')', '^', '2',
+	'^', '3'], ['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/',
 	'+']),
 ])
 def test_shuntingyard(test_input, expected):
